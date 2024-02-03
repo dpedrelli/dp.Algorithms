ca70c77135019f16bba9deb58bc80f3126acba4c
David Pedrelli <programming@ibis-nt.com>
2024-02-03 18:34:54 -0500

    Added bubble sort.

---
+++ dp.Algorithms/Sorters/BubbleSorter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms.Sorters
+{
+    public static class BubbleSorter
+    {
+        public static T[] SortArray<T>(T[] array) where T : struct, IComparable<T>
+        {
+            for (int i = 0; i < array.Length - 1; i++) 
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (array[j].CompareTo(array[j-1]) < 0)
+                    {
+                        HelperMethods.SwapArrayElements(array, j, j - 1);
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
