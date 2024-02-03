b7a29aa355402acd373a07dda0c4aafebc30f06c
David Pedrelli <programming@ibis-nt.com>
2024-02-03 18:45:12 -0500

    Added insertion sorting.

--- dp.Algorithms.Tests/SortingTests.cs
+++ dp.Algorithms.Tests/SortingTests.cs
@@ -25,5 +25,16 @@ namespace dp.Algorithms.Tests
             Assert.NotNull(result);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
+        [InlineData(new int[] { 52, 96, 67, 71, 42, 38, 39, 40, 14 }, new int[] { 14, 38, 39, 40, 42, 52, 67, 71, 96 })]
+        public void InsertionSorter_SortIntegerArray_SortsArray(int[] array, int[] expected)
+        {
+            var result = InsertionSorter.SortArray(array);
+
+            Assert.NotNull(result);
+            Assert.Equal(expected, result);
+        }
     }
 }
---
+++ dp.Algorithms/Sorters/InsertionSorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms.Sorters
+{
+    public static class InsertionSorter
+    {
+        public static T[] SortArray<T>(T[] array) where T : struct, IComparable<T>
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j].CompareTo(array[j - 1]) < 0)
+                    {
+                        HelperMethods.SwapArrayElements(array, j, j - 1);
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
