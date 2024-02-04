74653a2e156455c123537b5b95c99c6d328d29ec
David Pedrelli <programming@ibis-nt.com>
2024-02-03 19:42:57 -0500

    Added selection sorting, but not 100% reliable.

--- dp.Algorithms.Tests/SortingTests.cs
+++ dp.Algorithms.Tests/SortingTests.cs
@@ -36,5 +36,19 @@ namespace dp.Algorithms.Tests
             Assert.NotNull(result);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
+        [InlineData(new int[] { 52, 96, 67, 71, 42, 38, 39, 40, 14 }, new int[] { 14, 38, 39, 40, 42, 52, 67, 71, 96 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7 }, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [InlineData(new int[] { 7, 2, 3, 4, 5, 6, 1 }, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [InlineData(new int[] { 5, 6, 7, 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        public void SelectionSorter_SortIntegerArray_SortsArray(int[] array, int[] expected)
+        {
+            var result = SelectionSorter.SortArray(array);
+
+            Assert.NotNull(result);
+            Assert.Equal(expected, result);
+        }
     }
 }
---
+++ dp.Algorithms/Sorters/SelectionSorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms.Sorters
+{
+    public static class SelectionSorter
+    {
+        public static T[] SortArray<T>(T[] array) where T : struct, IComparable<T>
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var k = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(array[k]) < 0)
+                    {
+                        k = j;
+                    }
+                    HelperMethods.SwapArrayElements(array, i, k);
+                }
+            }
+            return array;
+        }
+    }
+}
