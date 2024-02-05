1b00bf267978351a9446aae3b30b26bddd641a8e
David Pedrelli <programming@ibis-nt.com>
2024-02-05 14:49:23 -0500

    Fibonacci generator

---
+++ dp.Algorithms.Tests/RecursionTests.cs
@@ -0,0 +1,42 @@
+﻿using dp.Algorithms.Recursion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace dp.Algorithms.Tests
+{
+    public class RecursionTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        public void Fibonacci_PassCount_ReturnsCorrect(int count, int expected)
+        {
+            var result = FibonacciGenerator.GenerateNumber(count);
+            Assert.Equal(expected, result);
+        }
+
+
+        [Theory]
+        [InlineData(0, new int[0])]
+        [InlineData(1, new int[1] { 0 })]
+        [InlineData(2, new int[2] { 0, 1 })]
+        [InlineData(3, new int[3] { 0, 1, 1 })]
+        [InlineData(4, new int[4] { 0, 1, 1, 2 })]
+        [InlineData(5, new int[5] { 0, 1, 1, 2, 3 })]
+        [InlineData(6, new int[6] { 0, 1, 1, 2, 3, 5 })]
+        public void FibonacciArray_PassCount_ReturnsCorrect(int count, int[] expected)
+        {
+            var result = FibonacciGenerator.GenerateArray(count);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ dp.Algorithms/Recursion/FibonacciGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms.Recursion
+{
+    public static class FibonacciGenerator
+    {
+        public static int GenerateNumber(int count)
+        {
+            if (count == 0)
+            {
+                return 0;
+            }
+            else if (count == 1)
+            {
+                return 1;
+            }
+            var value = GenerateNumber(count - 2) + GenerateNumber(count - 1);
+            return value;
+        }
+
+        public static int[] GenerateArray(int count)
+        {
+            var array = new int[count];
+            if (count < 1)
+            {
+                return array;
+            }
+            array[0] = 0;
+            if (count < 2)
+            {
+                return array;
+            }
+            array[1] = 1;
+            for (int i = 2; i < count; i++)
+            {
+                array[i] = array[i - 2] + array[i - 1];
+            }
+            return array;
+        }
+    }
+}
