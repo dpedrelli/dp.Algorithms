0a8a0f879f5ffd2799960e1450af2b3c8e7c4ceb
David Pedrelli <programming@ibis-nt.com>
2024-02-05 20:01:11 -0500

    Linked list.

---
+++ dp.Algorithms.Tests/LinkedListTester.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms.Tests
+{
+    public class LinkedListTester
+    {
+        [Fact]
+        public void LinkedList_RemoveEmpty_ReceiveError()
+        {
+            var list = new LinkedList<int>();
+            Assert.Throws<Exception>(() => list.RemoveFirst());
+        }
+
+        [Fact]
+        public void LinkedList_PeekEmpty_ReceiveError()
+        {
+            var list = new LinkedList<int>();
+            Assert.Throws<Exception>(() => list.Peek());
+        }
+
+        [Fact]
+        public void LinkedList_Prepend_ConfirmNode()
+        {
+            var list = new LinkedList<int>();
+            list.Prepend(10);
+            Assert.Equal(1, list.Size);
+            Assert.Equal(10, list.Head.Data);
+        }
+
+        [Fact]
+        public void LinkedList_PrependTwice_ConfirmNodes()
+        {
+            var list = new LinkedList<int>();
+            list.Prepend(10);
+            list.Prepend(11);
+            Assert.Equal(2, list.Size);
+            Assert.Equal(11, list.Head.Data);
+            Assert.Equal(10, list.Head.Next.Data);
+        }
+
+        [Fact]
+        public void LinkedList_PrependAndPeek_ConfirmNode()
+        {
+            var list = new LinkedList<int>();
+            list.Prepend(10);
+            Assert.Equal(1, list.Size);
+            Assert.Equal(10, list.Peek());
+        }
+
+        [Fact]
+        public void LinkedList_PrependAndRemove_ConfirmSize()
+        {
+            var list = new LinkedList<int>();
+            list.Prepend(10);
+            Assert.Equal(1, list.Size);
+            list.RemoveFirst();
+            Assert.Equal(0, list.Size);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(10)]
+        public void LinkedList_InsertAtInvalid_ReceiveError(int index)
+        {
+            var list = new LinkedList<int>();
+            Assert.Throws<Exception>(() => list.InsertAt(5, index));
+        }
+
+        [Fact]
+        public void LinkedList_InsertAt0_ConfirmNode()
+        {
+            var list = new LinkedList<int>();
+            list.InsertAt(10, 0);
+            Assert.Equal(1, list.Size);
+            Assert.Equal(10, list.Head.Data);
+        }
+
+        [Fact]
+        public void LinkedList_InsertAt3_ConfirmNode()
+        {
+            var list = new LinkedList<int>();
+            list.InsertAt(10, 0);
+            list.InsertAt(11, 1);
+            list.InsertAt(12, 2);
+            list.InsertAt(13, 3);
+            list.InsertAt(14, 4);
+            list.InsertAt(15, 5);
+            Assert.Equal(6, list.Size);
+            Assert.Equal(10, list.Head.Data);
+            Assert.Equal(11, list.Head.Next.Data);
+            Assert.Equal(12, list.Head.Next.Next.Data);
+            Assert.Equal(13, list.Head.Next.Next.Next.Data);
+            Assert.Equal(14, list.Head.Next.Next.Next.Next.Data);
+            Assert.Equal(15, list.Head.Next.Next.Next.Next.Next.Data);
+            list.InsertAt(16, 3);
+            Assert.Equal(16, list.Head.Next.Next.Next.Data);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(10)]
+        public void LinkedList_RemoveAtInvalid_ReceiveError(int index)
+        {
+            var list = new LinkedList<int>();
+            list.InsertAt(10, 0);
+            list.InsertAt(11, 1);
+            list.InsertAt(12, 2);
+            list.InsertAt(13, 3);
+            list.InsertAt(14, 4);
+            list.InsertAt(15, 5);
+            Assert.Throws<Exception>(() => list.RemoveAt(index));
+        }
+
+        [Fact]
+        public void LinkedList_RemoveAt3_ConfirmRemoved()
+        {
+            var list = new LinkedList<int>();
+            list.InsertAt(10, 0);
+            list.InsertAt(11, 1);
+            list.InsertAt(12, 2);
+            list.InsertAt(13, 3);
+            list.InsertAt(14, 4);
+            list.InsertAt(15, 5);
+            Assert.Equal(6, list.Size);
+            Assert.Equal(10, list.Head.Data);
+            Assert.Equal(11, list.Head.Next.Data);
+            Assert.Equal(12, list.Head.Next.Next.Data);
+            Assert.Equal(13, list.Head.Next.Next.Next.Data);
+            Assert.Equal(14, list.Head.Next.Next.Next.Next.Data);
+            Assert.Equal(15, list.Head.Next.Next.Next.Next.Next.Data);
+            var data = list.RemoveAt(3);
+            Assert.Equal(5, list.Size);
+            Assert.Equal(13, data);
+            Assert.Equal(14, list.Head.Next.Next.Next.Data);
+        }
+
+        [Fact]
+        public void LinkedList_RemoveData_ConfirmRemoved()
+        {
+            var list = new LinkedList<int>();
+            list.InsertAt(10, 0);
+            list.InsertAt(11, 1);
+            list.InsertAt(12, 2);
+            list.InsertAt(13, 3);
+            list.InsertAt(14, 4);
+            list.InsertAt(15, 5);
+            list.InsertAt(13, 6);
+            list.InsertAt(13, 7);
+            Assert.Equal(8, list.Size);
+            Assert.Equal(10, list.Head.Data);
+            Assert.Equal(11, list.Head.Next.Data);
+            Assert.Equal(12, list.Head.Next.Next.Data);
+            Assert.Equal(13, list.Head.Next.Next.Next.Data);
+            Assert.Equal(14, list.Head.Next.Next.Next.Next.Data);
+            Assert.Equal(15, list.Head.Next.Next.Next.Next.Next.Data);
+            Assert.Equal(13, list.Head.Next.Next.Next.Next.Next.Next.Data);
+            Assert.Equal(13, list.Head.Next.Next.Next.Next.Next.Next.Next.Data);
+            list.RemoveData(13);
+            Assert.Equal(5, list.Size);
+            Assert.Equal(14, list.Head.Next.Next.Next.Data);
+        }
+
+    }
+}
---
+++ dp.Algorithms/DataStructures/LinkedList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace dp.Algorithms
+{
+    public class LinkedNode<T> where T : IComparable<T>
+    {
+        public T? Data { get; set; }
+        public LinkedNode<T>? Next { get; set; }
+
+        public LinkedNode(T? data)
+        {
+            Data = data;
+        }
+    }
+
+    public class LinkedList<T> where T : IComparable<T>
+    {
+        public LinkedNode<T>? Head { get; set; }
+        private int _size;
+        public int Size { get { return _size; } }
+        public bool IsEmpty => _size == 0;
+
+        public void Prepend(T data)
+        {
+            var node = new LinkedNode<T>(data);
+            node.Next = Head;
+            Head = node;
+            _size++;
+        }
+
+        public T? RemoveFirst()
+        {
+            if (_size == 0 || Head == null)
+            {
+                throw new Exception("Invalid list size.");
+            }
+            var data = Head!.Data;
+            Head = Head.Next;
+            _size--;
+            return data;
+        }
+
+        public T? Peek()
+        {
+            if (_size == 0 || Head == null)
+            {
+                throw new Exception("Invalid list size.");
+            }
+            return Head!.Data;
+        }
+
+        public void InsertAt(T data, int index)
+        {
+            if (index < 0 || index > Size)
+            {
+                throw new Exception("Invalid insertion index.");
+            }
+            if (index == 0)
+            {
+                Prepend(data);
+                return;
+            }
+            var currentNode = Head;
+            var nextNode = Head?.Next;
+            var node = new LinkedNode<T>(data);
+            for (int i = 1; i < index; i++)
+            {
+                currentNode = nextNode;
+                nextNode = currentNode?.Next;
+            }
+            currentNode.Next = node;
+            node.Next = nextNode;
+            _size++;
+        }
+
+        public T? RemoveAt(int index)
+        {
+            if (index < 0 || index > Size - 1)
+            {
+                throw new Exception("Invalid removal index.");
+            }
+            if (index == 0)
+            {
+                return RemoveFirst();
+            }
+            var currentNode = Head;
+            var nextNode = Head?.Next;
+            for (int i = 1; i < index; i++)
+            {
+                currentNode = nextNode;
+                nextNode = nextNode?.Next;
+            }
+            currentNode.Next = nextNode?.Next;
+            _size--;
+            return nextNode.Data;
+        }
+
+        public void RemoveData(T? data)
+        {
+            var currentNode = Head;
+            var index = 0;
+            while (currentNode != null) 
+            {
+                if (HelperMethods.Is1EqualTo2(currentNode.Data, data))
+                {
+                    RemoveAt(index);
+                }
+                else
+                {
+                    index++;
+                }
+                currentNode = currentNode?.Next;
+            }
+        }
+    }
+}
