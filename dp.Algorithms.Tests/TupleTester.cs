9daff67528465a3f0996c676e0fef7389829223f
David Pedrelli <programming@ibis-nt.com>
2024-02-07 16:26:46 -0500

    Tuple

---
+++ dp.Algorithms.Tests/TupleTester.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms.Tests
+{
+    public class TupleTester
+    {
+        [Theory]
+        [InlineData("key", "value", "key", "value", true)]
+        [InlineData("key", "value", "key2", "value2", false)]
+        public void Equals_CreateTwoTuples_ConfirmEquality(object key1, object value1, object key2, object value2, bool expected) 
+        { 
+            var tuple1 = new Tuple(key1, value1);
+            var tuple2 = new Tuple(key2, value2);
+            Assert.Equal(expected, tuple1.Equals(tuple2));
+        }
+
+        [Fact]
+        public void ToString_CreateTuple_ConfirmToStringResult()
+        {
+            var tuple = new Tuple("key", "value");
+            Assert.Equal("(key, value)", tuple.ToString());
+        }
+    }
+}
---
+++ dp.Algorithms/DataStructures/Tuple.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dp.Algorithms
+{
+    public class Tuple
+    {
+        public object Key { get; set; }
+        public object Value { get; set; }
+
+        public Tuple(object key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString() 
+        { 
+            return "(" + Key + ", " + Value + ")";
+        }
+
+        public override bool Equals(object? other) 
+        { 
+            if (ReferenceEquals(null, other)) return false;
+            if (other.GetType() != typeof(Tuple)) return false;
+            Tuple o = (Tuple)other;
+            if (Key.Equals(o.Key)  && Value.Equals(o.Value))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
